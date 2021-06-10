d88685ee2cf4316b1246a67e18a2443b145793ca
vladimir99kuzmin <vladimir99kuzmin@outlook.com>
2021-06-10 23:52:59 +0700

    100621

--- main.js
+++ main.js
@@ -1,43 +1,51 @@
-let type = {
-    num: 1,
-    black: 2,
-    gray: 3
+let cellType = {
+    num: "num",
+    black: "black",
+    grey: "grey"
 }
 let board = document.getElementById("board");
-for (let i = 0; i <= 9; i++) {
-	for (let j = 0; j <=9; j++) {
-        if ((i === 0 || j === 0) || (i === 9 || j === 9)) {            
-            addCell(type.num);
-        }
-        else if(isOdd(i) ^ isOdd(j)) {
-            addCell(type.black);
-        }
-        else {
-            addCell(type.gray);
-        }
-  }
+let n = cellType.num,
+    b = cellType.black,
+    g = cellType.grey,
+    p = cellType.player;
+let map =
+    [
+        [n, n, n, n, n, n, n, n, n, n],
+        [n, g, b, g, b, g, b, g, b, n],
+        [n, b, g, b, g, b, g, b, g, n],
+        [n, g, b, g, b, g, b, g, b, n],
+        [n, b, g, b, g, b, g, b, g, n],
+        [n, g, b, g, b, g, b, g, b, n],
+        [n, b, g, b, g, b, g, b, g, n],
+        [n, g, b, g, b, g, b, g, b, n],
+        [n, b, g, b, g, b, g, b, g, n],
+        [n, n, n, n, n, n, n, n, n, n]
+    ];
+//Этот MAP можно получить при помощи цикла с XOR'ом влегкую, очевидно, но я наглядно хочу показать, что могу ее изменить в любом месте в любой момент (это может сгодиться для каких-нить других игрушек)
+
+function drawCell(type, posY, posX, inner = " ") {
+    board.innerHTML += "<div class=\"cell " + type + "\" style = \"top: " + (40 * posY) + "px; left: " + (40 * posX) + "px;\">" + inner + "</div>";
 }
-let numCells = document.getElementsByClassName("num");
-for (let i = 0; i <= 0; i++) {
-    if (i === 0 || i === 9) {
-        
+
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        switch (map[i][j]) {
+            case n:
+                let val = " ";
+                if (i == 0 && j != 0 && j != 9) {
+                    val = String.fromCharCode(48+j);
+                }
+                if (j == 0 && i != 0 && i != 9) {
+                    val = String.fromCharCode(64+i);
+                }
+                drawCell(n, i, j, val);
+                break;
+            case g:
+                drawCell(g, i, j);
+                break;
+            case b:
+                drawCell(b, i, j);
+                break;
+        }
     }
-}
-function addCell(cellType) {
-switch (cellType) {
-    case 1:
-        board.innerHTML += "<div class=\"cell num\"> </div>"
-        break;
-    case 2:
-        board.innerHTML += "<div class=\"cell black\"> </div>"
-        break;
-    case 3:
-        board.innerHTML += "<div class=\"cell gray\"> </div>"
-        break;
-    default:
-        break;
-}
-}
-function isOdd(number) {
-    return number%2 === 0;
 }
